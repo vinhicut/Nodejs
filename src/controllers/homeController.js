b2a2ce77b6838e5362fe1b2943c721579bf5a1af
Vinh Nguyen <nguyenvinhvan90@gmail.com>
2022-10-20 09:59:23 +0700

    xong #25

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from '../models/index'
+import CRUDService from "../services/CRUDService";
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
@@ -20,8 +21,9 @@ let getAboutPage = (req, res) => {
 let getCRUD = (req, res) => {
     return res.render('crud.ejs');
 }
-let postCRUD = (req, res) => {
-    console.log(req.body);
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message)
     return res.send('post crud form server ');
 
 }
---
+++ src/services/CRUDService.js
@@ -0,0 +1,41 @@
+import bcrypt from 'bcryptjs';
+import db from '../models/index';
+
+const salt = bcrypt.genSaltSync(10);
+
+let createNewUser = async (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPassWordFromBcrypt = await hashUserPassword(data.password)
+            await db.User.create({
+                email: data.email,
+                password: hashPassWordFromBcrypt,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                address: data.address,
+                phonenumber: data.phonenumber,
+                gender: data.gender === '1' ? true : false,
+                roleId: data.roleId,
+            })
+            resolve('ok create new user succeed')
+
+        } catch (e) {
+            reject(e);
+        }
+    })
+
+}
+let hashUserPassword = (password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPassword = await bcrypt.hashSync(password, salt);
+            resolve(hashPassword)
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+module.exports = {
+    createNewUser: createNewUser
+}
