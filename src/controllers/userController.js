ea3bb06600820201b96fd7d374e6eb398792fe61
Vinh Nguyen <nguyenvinhvan90@gmail.com>
2022-10-25 12:00:46 +0700

    Hien thi du lieu nguoi dung ra view

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -24,7 +24,7 @@ let handleLogin = async (req, res) => {
     })
 }
 let handleGetAllUsers = async (req, res) => {
-    let id = req.body.id; // ALL, ID
+    let id = req.query.id; // ALL, ID
     if (!id) {
         return res.status(200).json({
             errCode: 1,
