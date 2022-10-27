329468870abf3295065c4c222ef22894753c13db
Vinh Nguyen <nguyenvinhvan90@gmail.com>
2022-10-27 17:51:37 +0700

    them nguoi dung dung modal

--- src/services/userService.js
+++ src/services/userService.js
@@ -120,22 +120,23 @@ let createNewUser = (data) => {
                     errCode: 1,
                     errMessage: 'Your email is already in used, plz try another email'
                 })
+            } else {
+                let hashPassWordFromBcrypt = await hashUserPassword(data.password);
+                await db.User.create({
+                    email: data.email,
+                    password: hashPassWordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phonenumber: data.phonenumber,
+                    gender: data.gender === '1' ? true : false,
+                    roleId: data.roleId,
+                })
+                resolve({
+                    errCode: 0,
+                    errmessage: 'OK',
+                })
             }
-            let hashPassWordFromBcrypt = await hashUserPassword(data.password);
-            await db.User.create({
-                email: data.email,
-                password: hashPassWordFromBcrypt,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                address: data.address,
-                phonenumber: data.phonenumber,
-                gender: data.gender === '1' ? true : false,
-                roleId: data.roleId,
-            })
-            resolve({
-                errCode: 0,
-                message: 'OK',
-            })
 
         } catch (e) {
             reject(e);
